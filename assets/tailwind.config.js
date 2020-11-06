5e38b304ef404ea424f03399089d7177bd124c54
German Velasco <germsvel@gmail.com>
2020-11-06 05:10:02 -0500

    Install tailwind with webpack
    Thanks to the installation help from
    https://pragmaticstudio.com/tutorials/adding-tailwind-css-to-phoenix

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js"
+  ],
+  plugins: [
+    require('@tailwindcss/ui')
+  ]
+}
