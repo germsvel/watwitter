8b3a431f885cadd2b040e49c238d4b3bf8374e20
German Velasco <germsvel@gmail.com>
2020-11-23 05:52:18 -0500

    Scaffold PostComponent HTML markup
    We scaffold `PostComponent`s HTML to prepare to add the data we care
    about through tests.

--- lib/watwitter_web/live/post_component.ex
+++ lib/watwitter_web/live/post_component.ex
@@ -4,6 +4,31 @@ defmodule WatwitterWeb.PostComponent do
   def render(assigns) do
     ~L"""
     <div id="post-<%= @post.id %>" class="post">
+      <img class="avatar" src="">
+      <div class="post-content">
+        <div class="post-header">
+          <div class="post-user-info">
+            <span class="post-user-name">
+            </span>
+            <span class="post-user-username">
+            </span>
+          </div>
+
+          <div class="post-date-info">
+            <span class="post-date-separator">.</span>
+            <span class="post-date">
+            </span>
+          </div>
+        </div>
+
+        <div class="post-body">
+        </div>
+
+        <div class="post-actions">
+          <a class="post-action" href="#">
+          </a>
+        </div>
+      </div>
     </div>
     """
   end
