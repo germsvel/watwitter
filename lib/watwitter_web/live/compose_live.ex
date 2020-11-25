efd948a9b32909e60ac050f2ab142c55ed6a9441
German Velasco <germsvel@gmail.com>
2020-11-25 15:28:37 -0500

    Scaffold compose_live.html form
    Scaffold the post form in `compose_live.html.leex` to handle styling
    without the key functionality.

--- lib/watwitter_web/live/compose_live.ex
+++ lib/watwitter_web/live/compose_live.ex
@@ -1,5 +1,12 @@
 defmodule WatwitterWeb.ComposeLive do
   use WatwitterWeb, :live_view
 
+  alias Watwitter.Accounts
   alias WatwitterWeb.SVGHelpers
+
+  def mount(_params, session, socket) do
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+
+    {:ok, assign(socket, current_user: current_user)}
+  end
 end
