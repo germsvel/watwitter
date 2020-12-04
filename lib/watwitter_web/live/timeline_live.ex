b996777d983202fc1a9376503c6d1e0491a50c67
German Velasco <germsvel@gmail.com>
2020-12-04 08:50:03 -0500

    Fetch current user in TimelineLive
    We get the current user in `TimelineLive` by using the `user_token`
    found in the session. This current user will be needed for rendering the
    avatar on the home page.

--- lib/watwitter_web/live/timeline_live.ex
+++ lib/watwitter_web/live/timeline_live.ex
@@ -1,3 +1,11 @@
 defmodule WatwitterWeb.TimelineLive do
   use WatwitterWeb, :live_view
+
+  alias Watwitter.Accounts
+
+  def mount(_params, session, socket) do
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+
+    {:ok, assign(socket, current_user: current_user)}
+  end
 end
