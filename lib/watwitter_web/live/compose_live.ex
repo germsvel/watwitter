8763788f2ae4a24e4eb076f9b1212362f70fbd22
German Velasco <germsvel@gmail.com>
2020-12-31 13:18:33 -0500

    Test user can create posts
    What changed?
    ============
    
    We test (and implement) that a user can create a post.
    
    We see the importance of using the `form/3` helper with
    `render_submit/1`. If we use `render_submit/3` directly -- targeting the
    event and passing the form data -- it's possible to get false positives.
    
    We also see how to test the successful and the error case. One redirects
    us to the Timeline, the other we can simply make assertions about the
    text that is re-rendered in the ComposeLive

--- lib/watwitter_web/live/compose_live.ex
+++ lib/watwitter_web/live/compose_live.ex
@@ -2,11 +2,27 @@ defmodule WatwitterWeb.ComposeLive do
   use WatwitterWeb, :live_view
 
   alias Watwitter.Accounts
+  alias Watwitter.Timeline
+  alias Watwitter.Timeline.Post
   alias WatwitterWeb.SVGHelpers
 
   def mount(_params, session, socket) do
     current_user = Accounts.get_user_by_session_token(session["user_token"])
+    changeset = Timeline.change_post(%Post{})
 
-    {:ok, assign(socket, current_user: current_user)}
+    {:ok, assign(socket, changeset: changeset, current_user: current_user)}
+  end
+
+  def handle_event("save", %{"post" => post_params}, socket) do
+    current_user = socket.assigns.current_user
+    params = Map.put(post_params, "user_id", current_user.id)
+
+    case Timeline.create_post(params) do
+      {:ok, _post} ->
+        {:noreply, push_redirect(socket, to: Routes.timeline_path(socket, :index))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
---
+++ test/watwitter_web/live/compose_live_test.exs
@@ -0,0 +1,30 @@
+defmodule WatwitterWeb.ComposeLiveTest do
+  use WatwitterWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  setup :register_and_log_in_user
+
+  test "user can create a new post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    {:ok, _view, html} =
+      view
+      |> form("#new-post", post: %{body: "This is awesome"})
+      |> render_submit()
+      |> follow_redirect(conn, Routes.timeline_path(conn, :index))
+
+    assert html =~ "This is awesome"
+  end
+
+  test "user is notified if posting fails", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    rendered =
+      view
+      |> form("#new-post", post: %{body: nil})
+      |> render_submit()
+
+    assert rendered =~ "can&apos;t be blank"
+  end
+end
