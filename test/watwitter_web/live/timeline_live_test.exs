278f606946497782e994b11942a5f127489d1dd5
German Velasco <germsvel@gmail.com>
2021-07-23 10:35:45 -0400

    Add test with Floki to ensure new post position
    What changed?
    ============
    
    We add a test with Floki to show how we can assert the position of posts
    relative to each other.
    
    To see why that might be helpful, we consider a potential bug if we had
    accidentally appended the new post ids to the list of ids (instead of
    prepended).
    
    ```diff
       def handle_event("show-new-posts", _, socket) do
         {:noreply,
          socket
    -     |> update(:post_ids, fn ids -> socket.assigns.new_post_ids ++ ids end)
    +     |> update(:post_ids, fn ids -> ids ++ socket.assigns.new_post_ids end)
          |> assign(:new_post_ids, [])
          |> assign(:new_posts_count, 0)}
       end
    ```
    
    Our existing tests did not enforce that the new posts should be listed
    before the older posts. With Floki, we were able to fetch all the posts
    and look at their relative positions.

--- test/watwitter_web/live/timeline_live_test.exs
+++ test/watwitter_web/live/timeline_live_test.exs
@@ -89,6 +89,22 @@ defmodule WatwitterWeb.TimelineLiveTest do
     refute view |> new_posts_notice() |> has_element?()
   end
 
+  test "user sees new posts at the top of timeline", %{conn: conn} do
+    previous_post = insert(:post)
+    {:ok, view, _html} = live(conn, "/")
+    new_post = insert(:post)
+    Timeline.broadcast_post_created(new_post)
+
+    rendered =
+      view
+      |> new_posts_notice("Show 1 post")
+      |> render_click()
+
+    [top_card, bottom_card] = all_post_cards(rendered)
+    assert element_text(top_card) =~ new_post.body
+    assert element_text(bottom_card) =~ previous_post.body
+  end
+
   test "updates rendered post when receiving a post-update message", %{conn: conn} do
     post = insert(:post, likes_count: 0)
     {:ok, view, _html} = live(conn, "/")
@@ -110,6 +126,14 @@ defmodule WatwitterWeb.TimelineLiveTest do
     assert view |> timeline_post(newest) |> has_element?()
     assert view |> timeline_post(oldest) |> has_element?()
   end
+
+  defp all_post_cards(html) do
+    html
+    |> Floki.parse_document!()
+    |> Floki.find("[data-role='post']")
+  end
+
+  defp element_text(el), do: Floki.text(el)
 
   defp update_post(post, changes) do
     post
